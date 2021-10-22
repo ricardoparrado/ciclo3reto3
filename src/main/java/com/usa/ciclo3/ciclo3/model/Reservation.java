3cfed249d4b9f31328c21094bd9f383bf0cb98a0
ricardoparrado <91809858+ricardoparrado@users.noreply.github.com>
2021-10-21 23:40:18 -0500

    Add files via upload

--- src/main/java/com/usa/ciclo3/ciclo3/model/Reservation.java
+++ src/main/java/com/usa/ciclo3/ciclo3/model/Reservation.java
@@ -17,7 +17,7 @@ public class Reservation implements Serializable {
     private Integer idReservation;
     private Date startDate;
     private Date devolutionDate;
-    private String status = "Created";
+    private String status = "created";
 
     @ManyToOne
     @JoinColumn(name = "machine")
